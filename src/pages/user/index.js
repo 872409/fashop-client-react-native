f37c58796ec075e14ed97e7f7ece2896012d0faf
韩文博 <9476400@qq.com>
2018-11-26 16:45:51 +0800

    fixed

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -7,7 +7,7 @@ import {
     TouchableOpacity
 } from 'react-native';
 import { List } from "antd-mobile-rn";
-import { PublicStyles } from '../../utils/publicStyleModule';
+import { PublicStyles, windowHeight, windowWidth } from '../../utils/publicStyleModule';
 import Avatar from "../../components/public/avatar";
 import EntypoIcon from "react-native-vector-icons/Entypo";
 import { connect } from "react-redux";
@@ -15,18 +15,23 @@ import { connect } from "react-redux";
 const Item = List.Item
 
 @connect(
-    ({ app: { user: {
-        login,
-        userInfo,
-    }}}) => ({
+    ({
+         app: {
+             user: {
+                 login,
+                 userInfo,
+             }
+         }
+     }) => ({
         login,
         userInfo,
     }),
 )
 export default class User extends Component {
-    goOrderList(){
+    goOrderList() {
         this.props.navigation.navigate('OrderList')
     }
+
     render() {
         return <View style={PublicStyles.ViewMax}>
             {
@@ -82,31 +87,31 @@ export default class User extends Component {
             {
                 img: require('../../images/user/state_new.png'),
                 title: '待付款',
-                link:()=>{
-                    this.props.navigation.navigate('OrderList',{state_type:'state_new'})
+                link: () => {
+                    this.props.navigation.navigate('OrderList', { state_type: 'state_new' })
                 }
             }, {
                 img: require('../../images/user/state_pay.png'),
                 title: '待发货',
-                link:()=>{
-                    this.props.navigation.navigate('OrderList',{state_type:'state_pay'})
+                link: () => {
+                    this.props.navigation.navigate('OrderList', { state_type: 'state_pay' })
                 }
             }, {
                 img: require('../../images/user/state_send.png'),
                 title: '已完成',
-                link:()=>{
-                    this.props.navigation.navigate('OrderList',{state_type:'state_success'})
+                link: () => {
+                    this.props.navigation.navigate('OrderList', { state_type: 'state_success' })
                 }
             }, {
                 img: require('../../images/user/state_unevaluate.png'),
                 title: '待评价',
-                link:()=>{
+                link: () => {
                     this.props.navigation.navigate('EvaluateList')
                 }
             }, {
                 img: require('../../images/user/state_refund.png'),
                 title: '退款售后',
-                link:()=>{
+                link: () => {
                     this.props.navigation.navigate('RefundList')
                 }
             }
@@ -157,24 +162,33 @@ export default class User extends Component {
             }
         ]
         return (
-            <List>
-                {
-                    botList.map((item, index) => (
-                        <Item
-                            key={index}
-                            arrow="horizontal"
-                            onClick={() => {
-                                item.path()
-                            }}
-                        >
-                            <View style={PublicStyles.rowCenter}>
-                                <Image style={styles.botImg} source={item.img} />
-                                <Text style={PublicStyles.title}>{item.title}</Text>
-                            </View>
-                        </Item>
-                    ))
-                }
-            </List>
+            <View style={PublicStyles.ViewMax}>
+                <List style={PublicStyles.ViewMax}>
+                    {
+                        botList.map((item, index) => (
+                            <Item
+                                key={index}
+                                arrow="horizontal"
+                                onClick={() => {
+                                    item.path()
+                                }}
+                            >
+                                <View style={PublicStyles.rowCenter}>
+                                    <Image style={styles.botImg} source={item.img} />
+                                    <Text style={PublicStyles.title}>{item.title}</Text>
+                                </View>
+                            </Item>
+                        ))
+                    }
+                </List>
+                <View style={styles.fashopCopyright}>
+                    <View style={styles.fashopCopyrightBody}>
+                        <Image source={require('../../images/fashop/copyright.png')} mode="aspectFit"
+                               style={styles.fashopCopyrightImg} />
+                        <Text style={styles.fashopCopyrightText}>提供技术支持</Text>
+                    </View>
+                </View>
+            </View>
         )
     }
 }
@@ -205,4 +219,26 @@ const styles = StyleSheet.create({
         height: 22,
         marginRight: 10
     },
+    fashopCopyright: {
+        bottom: 0,
+        width: windowWidth,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    fashopCopyrightBody: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    fashopCopyrightImg: {
+        height: 16,
+        width: 50,
+        marginRight: 5
+    },
+    fashopCopyrightText: {
+        color: '#cccccc',
+        fontSize: 12,
+        lineHeight: 12,
+        paddingBottom: 15,
+        marginTop: 5
+    }
 });
