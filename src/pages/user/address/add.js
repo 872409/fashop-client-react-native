35f6d0ad7b2b277c68c1ba9ed960300348fec3a3
刘金萌 <1321839748ljm@gmail.com>
2019-01-15 10:53:34 +0800

    style: 改进 地址添加代码格式 :art:​

--- src/pages/user/address/add.js
+++ src/pages/user/address/add.js
@@ -3,6 +3,7 @@ import {
     StyleSheet,
     View,
     SafeAreaView,
+    ScrollView
 } from 'react-native';
 import fa from '../../../utils/fa'
 import AddressModel from '../../../models/address'
@@ -11,6 +12,7 @@ import { Button } from 'antd-mobile-rn';
 import { Field } from '../../../components'
 import arrayTreeFilter from "array-tree-filter";
 import { StackActions } from "react-navigation";
+import { PublicStyles } from '../../../utils/style';
 
 const addressModel = new AddressModel()
 const areaModel = new AreaModel()
@@ -78,7 +80,7 @@ export default class UserAddressAdd extends Component {
         })
     }
 
-    async onSubmit() {
+    onSubmit = async() => {
         const { truename, mobile_phone, area_id, address, is_default, type } = this.state
         if (!truename) {
             return fa.toast.show({ title: '请输入姓名' })
@@ -125,63 +127,72 @@ export default class UserAddressAdd extends Component {
             areaList,
             onLoaded
         } = this.state
-        return onLoaded ? [<View style={{ flex: 1 }}>
-            <View style={{ backgroundColor: '#fff' }}>
-                <Field
-                    title="收货人："
-                    placeholder="请输入姓名"
-                    focus={true}
-                    value={truename}
-                    onChange={(e) => {
-                        this.onTruenameChange(e)
-                    }}
+        if (!onLoaded){
+            return null
+        }
+        console.log("areaList", areaList);
+        
+        return <View style={PublicStyles.ViewMax}>
+            <ScrollView>
+                <View style={{ backgroundColor: '#fff' }}>
+                    <Field
+                        title="收货人："
+                        placeholder="请输入姓名"
+                        focus={true}
+                        value={truename}
+                        onChange={(e) => {
+                            this.onTruenameChange(e)
+                        }}
+                    />
+                    <Field
+                        title="联系方式："
+                        inputType="numeric"
+                        placeholder="请输入手机号"
+                        value={mobile_phone}
+                        onChange={(e) => {
+                            this.onMobilePhoneChange(e)
+                        }}
+                    />
+                    <Field
+                        title="所在地区："
+                        type={'area'}
+                        areaList={areaList}
+                        value={[]}
+                        areaNames={combine_detail ? combine_detail : '请选择地区'}
+                        onChange={(e) => {
+                            this.onAreaChange(e)
+                        }}
+                    />
+                    <Field
+                        title="详细地址："
+                        value={address}
+                        placeholder="填写楼栋楼层或房间号信息"
+                        onChange={(e) => {
+                            this.onAddressChange(e)
+                        }}
+                    />
+                    <Field
+                        title="设置默认地址："
+                        desc="注：每次下单时会使用该地址"
+                        type={'switch'}
+                        checked={is_default === 1}
+                        onChange={(e) => {
+                            this.onIsDefaultChange(e)
+                        }}
+                    />
+                </View>
+            </ScrollView>
+            <SafeAreaView style={styles.buttonArea}>
+                <Button 
+                    style={{ borderRadius: 0, flex: 1 }} 
+                    type='warning' 
+                    size="large" 
+                    onClick={this.onSubmit}
                 >
-                </Field>
-                <Field
-                    title="联系方式："
-                    inputType="numeric"
-                    placeholder="请输入手机号"
-                    value={mobile_phone}
-                    onChange={(e) => {
-                        this.onMobilePhoneChange(e)
-                    }}
-                >2
-                </Field>
-                <Field
-                    title="所在地区："
-                    type={'area'}
-                    areaList={areaList}
-                    value={[]}
-                    areaNames={combine_detail ? combine_detail : '请选择地区'}
-                    onChange={(e) => {
-                        this.onAreaChange(e)
-                    }}
-                >
-                </Field>
-                <Field
-                    title="详细地址："
-                    value={address}
-                    placeholder="填写楼栋楼层或房间号信息"
-                    onChange={(e) => {
-                        this.onAddressChange(e)
-                    }}
-                >
-                </Field>
-                <Field
-                    title="设置默认地址："
-                    desc="注：每次下单时会使用该地址"
-                    type={'switch'}
-                    checked={is_default === 1}
-                    onChange={(e) => {
-                        this.onIsDefaultChange(e)
-                    }}
-                >
-                </Field>
-            </View>
-        </View>, <SafeAreaView style={styles.buttonArea}>
-            <Button style={{ borderRadius: 0, flex: 1 }} type={'warning'} size="large" onClick={() => {
-                this.onSubmit()
-            }}>保存</Button></SafeAreaView>] : null
+                    保存
+                </Button>
+            </SafeAreaView> 
+        </View>
     }
 }
 const styles = StyleSheet.create({
