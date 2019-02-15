2337ffb813c29e786e43e520d991961c135c75ce
刘金萌 <1321839748ljm@gmail.com>
2019-02-15 12:05:49 +0800

    delete unused api

--- src/config/api/goodsCategory.js
+++
@@ -1,9 +0,0 @@
-import { env } from '../index';
-
-const ROOT_URL = `${env.apiHost}/server/`;
-export const GoodsCategoryApi = {
-    list: {
-        url: `${ROOT_URL}goodscategory/list`,
-        method: 'GET'
-    },
-}
--- src/config/api/payment.js
+++
@@ -1,9 +0,0 @@
-import { env } from '../index';
-
-const ROOT_URL = `${env.apiHost}/server/`;
-export const PaymentApi = {
-    list: {
-        url: `${ROOT_URL}payment/list`,
-        method: 'GET'
-    },
-}
