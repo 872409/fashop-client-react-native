a673ae6f5d81ca9fcc9c20f20b365f823f91adfb
刘金萌 <1321839748ljm@gmail.com>
2019-02-20 10:35:06 +0800

    style: goods item title price

--- src/components/goods/item.js
+++ src/components/goods/item.js
@@ -45,7 +45,7 @@ const styles = StyleSheet.create({
     },
     title: {
         marginVertical: 5,
-        fontSize: 12,
+        fontSize: 14,
         fontWeight: '400',
         lineHeight: 20,
         height: 40
@@ -53,5 +53,7 @@ const styles = StyleSheet.create({
     price: {
         color: ThemeStyle.ThemeColor,
         marginBottom: 10,
+        fontWeight: '500',
+        fontFamily: 'PingFangSC-Medium',
     },
 });
