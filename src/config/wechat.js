9b2b4212e01ca63f59b45d31d0592e02eb0c0d18
刘金萌 <1321839748ljm@gmail.com>
2019-02-22 10:48:25 +0800

    no message

--- src/config/wechat.js
+++ src/config/wechat.js
@@ -1,5 +1,7 @@
 const AppID = "wx1a76ca32a2015cda";
 const AppSecret = "047eae21f5bc3f7972bacb9b4a5ceaf5";
+// const debugSignature = "7b27e420d2289e0a845fb36cf7597fb0";
+// const releaseSignature = "c9b5ae6301d861909168b53c19db9498";
 // gh_cbed9edbbc7d  哪儿哪儿 mini 原始ID
 // gh_0a1a182155f9  Fashop mini 原始ID
 export {
